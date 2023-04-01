2335ab4853b31f0db14bb75a4021dc7b5469570f
Gerardo Jaime <gjaimeescareno@gmail.com>
2023-03-31 17:53:02 -0700

    Add in fucntion get_links_matches the module requests

--- beautifulsoup_utils.py
+++ beautifulsoup_utils.py
@@ -1,6 +1,7 @@
+from requests_utils import make_request
+from bs4 import BeautifulSoup
 import datetime
 import re
-from bs4 import BeautifulSoup
 
 def get_current_date(format):
     now = datetime.datetime.now()
@@ -11,29 +12,33 @@ def parse_html(html, parser='html.parser'):
     soup = BeautifulSoup(html, parser)
     return soup
 
-def get_links_matches(html):
-    # Parse the HTML
-    soup = parse_html(html)
-    match_links = []
+def get_links_matches(url):
+    # Make a request
+    content, status_code = make_request(url)
 
-    # Find all 'tr' elements with class 'livecomp'
-    date = get_current_date('%A, %d %B %Y')
-    livecomp = soup.find_all('tr', {'class': 'livecomp'})
+    if status_code == 200:
+        # Parse the HTML
+        soup = parse_html(content)
+        match_links = []
 
-    # Loop through each 'tr' element with class 'livecomp'
-    for lc in livecomp:
-        # Check if the date matches the current date
-        if re.search(date, str(lc)):
-            # Loop through the siblings of the livecomp row
-            for sibling in lc.find_next_siblings():
-                 # Check if the sibling is another livecomp row
-                if sibling.has_attr('class') and 'livecomp' in sibling['class']:
-                    # If it is, break out of the loop and move to the next livecomp row
-                    break
-                # If the sibling is a matchrow, append it to the list of match rows
-                elif sibling.has_attr('class') and 'matchrow' in sibling['class']:
-                    td = sibling.find_all('td')[2]  # Get the third td element
-                    link = td.find('a')  # Find the first a element
-                    href = link.get('href')  # Get the href attribute
-                    match_links.append(href)
-    return match_links
+        # Find all 'tr' elements with class 'livecomp'
+        date = get_current_date('%A, %d %B %Y')
+        livecomp = soup.find_all('tr', {'class': 'livecomp'})
+
+        # Loop through each 'tr' element with class 'livecomp'
+        for lc in livecomp:
+            # Check if the date matches the current date
+            if re.search(date, str(lc)):
+                # Loop through the siblings of the livecomp row
+                for sibling in lc.find_next_siblings():
+                    # Check if the sibling is another livecomp row
+                    if sibling.has_attr('class') and 'livecomp' in sibling['class']:
+                        # If it is, break out of the loop and move to the next livecomp row
+                        break
+                    # If the sibling is a matchrow, append it to the list of match rows
+                    elif sibling.has_attr('class') and 'matchrow' in sibling['class']:
+                        td = sibling.find_all('td')[2]  # Get the third td element
+                        link = td.find('a')  # Find the first a element
+                        href = link.get('href')  # Get the href attribute
+                        match_links.append(url[:28]+href)
+        return match_links
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from logging_utils import configure_logging
-from requests_utils import make_request
 import beautifulsoup_utils
 
 # Set up logging
 configure_logging('app.log')
 
-# Make a request
 URL = 'https://www.livesoccertv.com/competitions/mexico/primera-division/'
-content, status_code = make_request(URL)
 
-if status_code == 200:
-    links_matches = [URL[:28]+match for match in beautifulsoup_utils.get_links_matches(content)]
-    print(links_matches)
+links_matches = beautifulsoup_utils.get_links_matches(URL)
