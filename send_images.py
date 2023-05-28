07e78708064e3a7a09b4b8b768f1fcb7c88f59f8
Gerardo Jaime <gjaimeescareno@gmail.com>
2023-05-27 21:58:11 -0700

    Fix send_images

--- send_images.py
+++ send_images.py
@@ -5,18 +5,28 @@ from settings import PATH_SCHEDULES, text_post
 import time
 
 # get date today
-today = get_current_date_by_format(days=2)
+today = get_current_date_by_format()
 # find images with match date today
 images = find_images_with_suffix(PATH_SCHEDULES, f'{today}.png')
+print(images)
+
+# text for posts
+text_post_by_competition = {'europa':'CARTELERA EUROPEA',
+                            'north_america': 'CARTELERA NORTEAMERICANA',
+                            'south_america': 'CARTELERA SUDAMERICANA',
+                            'international': 'CARTELERA INTERNACIONAL'}
+
 
 def run():
     for path in images:
-        if path != None:
-            # post tweet
-            #create_tweet_with_media(text_post(f'{today} | CARTELERA EUROPEA'), path)
-            # send message to Telegram
-            send_image_with_msg(text_post(f'{today} | CARTELERA EUROPEA'), path)
-            time.sleep(1)
+        for key, value in text_post_by_competition.items():
+            if key in path.lower():
+                # post tweet
+                #create_tweet_with_media(text_post(f'{today} | {value}'), path)
+                # send message to Telegram
+                send_image_with_msg(text_post(f'{today} | {value}'), path)
+                time.sleep(1)
+                break
             
 
 if __name__ == '__main__':
